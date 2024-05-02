5f09ebb07f6fc0ed7b8e0fdb76d17a40a9d4052a
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-05-02 11:55:01 +0530

    tst

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import { BrowserRouter as Router,Routes, Route} from "react-router-dom";
+import Smartphone from "./Components/Smartphone";
 function App() {
   return (
     <div>
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/Home" Component={Home} />
           <Route path="/About" Component={About} />
+          <Route path="/Smartphone" Component={Smartphone} />
         </Routes>
         
       </Router>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,48 +1,73 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 // import { useEffect } from 'react';
 // import $ from 'jquery'; // import jQuery
+import Dropdown from "react-bootstrap/Dropdown";
 const Navbar = () => {
-    // useEffect(() => {
-    //     // Initialize dropdown when component mounts
-    //     $('.dropdown-toggle').dropdown();
-    //   }, []);
+  // useEffect(() => {
+  //     // Initialize dropdown when component mounts
+  //     $('.dropdown-toggle').dropdown();
+  //   }, []);
   return (
     <div>
       <nav className="navbar bg-dark navbar-expand-lg">
-  <Link className="navbar-brand text-white" to="/">Electronics</Link>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav ml-auto">
-      <li className="nav-item active">
-        <Link className="nav-link text-white" to="/Home">Home <span className="sr-only">(current)</span></Link>
-      </li>
-      <li>
-        <Link className="nav-link text-white" to='/About'>About</Link>
-      </li>
-      <li>
-      <div className="dropdown">
-        <button className="btn btn-info dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">
-            Categories
+        <Link className="navbar-brand text-white" to="/">
+          Electronics
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="dropdown-menu">
-            <Link className="dropdown-item" to="/Smartphones">Smartphones</Link>
-            <Link className="dropdown-item" to="/Television">Television</Link>
-            <Link className="dropdown-item" to="/Microwave">Microwave</Link>
-        </div>    
-      </div>
-      </li>
-    </ul>
-    <form className="form-inline my-2 my-lg-0">
-      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-    </div>
-  )
-}
 
-export default Navbar
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <Link className="nav-link text-white" to="/Home">
+                Home <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link text-white" to="/About">
+                About
+              </Link>
+            </li>
+            <li>
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Catogories
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="/Smartphone">Smartphone</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Another action
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                    Something else
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
