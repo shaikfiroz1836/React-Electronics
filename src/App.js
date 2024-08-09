b92fd0c9d69e9c33a0fcf2ebec3d24edaffdb68f
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-08-09 17:05:12 +0530

    tst

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import About from "./Components/About";
-import { BrowserRouter as Router,Routes, Route,Navigate} from "react-router-dom";
 import Smartphone from "./Components/Smartphone";
 import Iphone15 from "./Components/Iphone15";
 import Samsungs from "./Components/Samsungs";
 import Television from "./Components/Television";
 import Login from "./Components/Login";
-import React, { useState } from 'react';
-
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   const handleLogin = () => {
     setIsLoggedIn(true);
   };
 
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  useEffect(() => {
+    document.body.className = isDarkMode ? "dark-mode" : "light-mode";
+  }, [isDarkMode]);
+
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={isLoggedIn ? <Navigate to="/Home" /> : <Login onLogin={handleLogin} />} />
-        <Route path="/Home" Component={Home} />
-        <Route path="/About" Component={About} />
-        <Route path="/Smartphone" Component={Smartphone} />
-        <Route path="/Television" Component={Television} />
-        <Route path="/Iphone"  Component={Iphone15}/>
-        <Route path="/samsungs" Component={Samsungs} />
-        <Route path="/Login" element={<Login onLogin={handleLogin} />} />
-      </Routes>
-    </Router>
+    <div className={isDarkMode ? "dark-mode" : "light-mode"}>
+      <Router>
+        <Navbar isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
+        <Routes>
+          <Route path="/" element={isLoggedIn ? <Navigate to="/Home" /> : <Login onLogin={handleLogin} />} />
+          <Route path="/Home" Component={Home} />
+          <Route path="/About" Component={About} />
+          <Route path="/Smartphone" Component={Smartphone} />
+          <Route path="/Television" Component={Television} />
+          <Route path="/Iphone" Component={Iphone15} />
+          <Route path="/samsungs" Component={Samsungs} />
+          <Route path="/Login" element={<Login onLogin={handleLogin} />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
-  export default App;
+export default App;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,94 +1,80 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Dropdown from "react-bootstrap/Dropdown";
-const Navbar = () => {
- 
+import { FaSun, FaMoon } from "react-icons/fa";
+
+const Navbar = ({ isDarkMode, toggleTheme }) => {
   return (
-    <div>
-      {/* <nav className="navbar bg-dark navbar-expand-lg"> */}
-        {/* <Link className="navbar-brand text-white" to="/">
-          Electronics
-        </Link> */}
-        {/* <Link className="navbar-brand text-white" to="/">
-          <img height={150} width={150} src="/Photos/LOGO/Urbanmart3-NOBG.png" alt="Urbanmart3-NOBG Image"/>
-        </Link> */}
-        
-        <nav className="navbar bg-dark navbar-expand-lg" style={{ padding: '10px 15px' }}>
-        <Link className="navbar-brand text-white" to="/" style={{ display: 'flex', alignItems: 'center' }}>
-          <div style={{ height: 50, width: 50 }}>
-            <img
-              src="/Photos/LOGO/Urbanmart3-NOBG.png"
-              alt="Urbanmart3-NOBG Image"
-              style={{ height: '100%', width: '100%', objectFit: 'contain' }}
-            />
-          </div>
-        </Link>
-        
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+    <nav className="navbar navbar-expand-lg" style={{ backgroundColor: isDarkMode ? '#333' : '#f8f9fa' }}>
+      <Link className={`navbar-brand ${isDarkMode ? 'text-white' : 'text-dark'}`} to="/">
+        Electronics
+      </Link>
+      
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-              <Link className="nav-link text-white" to="/Home">
-                Home <span className="sr-only">(current)</span>
-              </Link>
-            </li>
-            <li>
-              <Link className="nav-link text-white" to="/About">
-                About
-              </Link>
-            </li>
-            <li>
-              <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                  Categories
-                </Dropdown.Toggle>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <Link className={`nav-link ${isDarkMode ? 'text-white' : 'text-dark'}`} to="/Home">
+              Home <span className="sr-only">(current)</span>
+            </Link>
+          </li>
+          <li>
+            <Link className={`nav-link ${isDarkMode ? 'text-white' : 'text-dark'}`} to="/About">
+              About
+            </Link>
+          </li>
+          <li>
+            <Dropdown>
+              <Dropdown.Toggle variant={isDarkMode ? "dark" : "light"} id="dropdown-basic">
+                Categories
+              </Dropdown.Toggle>
 
-                <Dropdown.Menu>
-                  <Dropdown.Item>
-                    <Link className="dropdown-item" to="/Smartphone">
-                      Smartphone
-                    </Link>
-                  </Dropdown.Item>
-                  <Dropdown.Item>
-                    <Link className="dropdown-item" to="/Television">
-                      Television
-                    </Link>
-                  </Dropdown.Item>
-                  <Dropdown.Item>
-                    <Link className="dropdown-item" to="/Microwave">
-                      Microwave
-                    </Link>
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </li>
-          </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <button
-              className="btn btn-outline-success my-2 my-sm-0"
-              type="submit"
-            >
-              Search
+              <Dropdown.Menu>
+                <Dropdown.Item>
+                  <Link className="dropdown-item" to="/Smartphone">
+                    Smartphone
+                  </Link>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <Link className="dropdown-item" to="/Television">
+                    Television
+                  </Link>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <Link className="dropdown-item" to="/Microwave">
+                    Microwave
+                  </Link>
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </li>
+          <li className="nav-item">
+            <button onClick={toggleTheme} className="btn btn-link">
+              {isDarkMode ? <FaSun color="yellow" /> : <FaMoon color="blue" />}
             </button>
-
-            {/* <Link className="nav-link text-white" to="/Login">
-                Login <span className="sr-only">(current)</span>
-              </Link> */}
-          </form>
-        </div>
-      </nav>
-    </div>
+          </li>
+        </ul>
+        <form className="form-inline my-2 my-lg-0">
+          <button
+            className={`btn ${isDarkMode ? 'btn-outline-light' : 'btn-outline-dark'} my-2 my-sm-0`}
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
   );
 };
 
